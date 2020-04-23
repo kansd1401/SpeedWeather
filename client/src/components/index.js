4a23c6054a48b693eb57be02a1185a0870f93ee4
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-22 19:18:34 -0700

    created a style for the map and added the link

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,20 +1,17 @@
-import React,{useState, useState} from 'react'
+import React,{useState} from 'react'
 import ReactMapGL from "react-map-gl"
 
 export default function Weather() {
   //longitude and latitude of bc so we can set that in the viewport of the map
   const [viewport,setViewport] = useState({
     latitude:	53.726669,
-    longitude: -127.647621
+    longitude: -127.647621,
+    width: "100vw",
+    height: "100vh",
+    zoom: 5
   })
   return (
-  <ReactMapGL
-    id="script-loader"
-    googleMapsApiKey="AIzaSyDuHIRR0pMmreEoEFimNbRkjWxTGCMEuMo">
-    <GoogleMap
-      id='example-map'
-    >
-      ...Your map components
-    </GoogleMap>
-  </ReactMapGL>)
+  <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+  onViewportChange={viewport => setViewport(viewport)}
+  mapStyle="mapbox://styles/kansd1401/ck9c4xsfg07sc1io0kb0jsvi0"> </ReactMapGL>)
 }
