79945ab9d5fa044779ff4ef4c4bfe9bcd0785c1f
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-22 19:35:27 -0700

    able to add markers

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,11 +1,7 @@
 import React,{useState} from 'react'
-import ReactMapGL from "react-map-gl"
+import ReactMapGL,{Marker} from "react-map-gl"
 
-
-
-
-Creston
-const locations = [{name: "Dease Lake", latitude: 58.472618, longitude: -130.016815},{name: "Fort Nelson", latitude: 58.805016, longitude: -122.697235},{name: "Terrace", latitude: 54.518829, longitude: -128.611404},{name: "Prince George", latitude: 53.912581, longitude: -122.747775},{name: "Whistler", latitude: 50.116003, longitude: 122.959426},{name: "Revelstoke", latitude: 50.997047, longitude:-118.197822},{name: "Creston", latitude: 49.095543 , longitude: -116.503134}]
+const locations = [{name: "Dease Lake",id: 1, latitude: 58.472618, longitude: -130.016815},{name: "Fort Nelson",id: 2, latitude: 58.805016, longitude: -122.697235},{name: "Terrace",id: 3, latitude: 54.518829, longitude: -128.611404},{name: "Prince George",id: 4, latitude: 53.912581, longitude: -122.747775},{name: "Whistler",id: 5, latitude: 50.116003, longitude: 122.959426},{name: "Revelstoke",id: 6, latitude: 50.997047, longitude:-118.197822},{name: "Creston",id: 7, latitude: 49.095543 , longitude: -116.503134}]
 
 export default function Weather() {
   //longitude and latitude of bc so we can set that in the viewport of the map
@@ -17,7 +13,13 @@ export default function Weather() {
     zoom: 5
   })
   return (
-  <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-  onViewportChange={viewport => setViewport(viewport)}
-  mapStyle="mapbox://styles/kansd1401/ck9c4xsfg07sc1io0kb0jsvi0"> </ReactMapGL>)
+    <ReactMapGL {...viewport} 
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={viewport => setViewport(viewport)}
+      mapStyle="mapbox://styles/kansd1401/ck9c4xsfg07sc1io0kb0jsvi0">
+        {locations.map( location => 
+          <Marker key={location.id} latitude={location.latitude} longitude={location.longitude}> 
+            <div>{location.name}</div>
+          </Marker>)}
+    </ReactMapGL>)
 }
