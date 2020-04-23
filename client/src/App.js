b382278c6f78803278aae999a932b01423b92df1
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-22 18:27:09 -0700

    changed map framework from google maps to mapbo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Weather from "./components"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+        <Weather/>
     </div>
   );
 }
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React,{useState, useState} from 'react'
+import ReactMapGL from "react-map-gl"
 
 export default function Weather() {
-
-  return 
-  (<div>
-    
-  </div>)
+  //longitude and latitude of bc so we can set that in the viewport of the map
+  const [viewport,setViewport] = useState({
+    latitude:	53.726669,
+    longitude: -127.647621
+  })
+  return (
+  <ReactMapGL
+    id="script-loader"
+    googleMapsApiKey="AIzaSyDuHIRR0pMmreEoEFimNbRkjWxTGCMEuMo">
+    <GoogleMap
+      id='example-map'
+    >
+      ...Your map components
+    </GoogleMap>
+  </ReactMapGL>)
 }
