716d6c1d3203d4ab51680a97912e585e33bc2094
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-22 19:28:59 -0700

    added coordinates fo each city

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,12 @@
 import React,{useState} from 'react'
 import ReactMapGL from "react-map-gl"
 
+
+
+
+Creston
+const locations = [{name: "Dease Lake", latitude: 58.472618, longitude: -130.016815},{name: "Fort Nelson", latitude: 58.805016, longitude: -122.697235},{name: "Terrace", latitude: 54.518829, longitude: -128.611404},{name: "Prince George", latitude: 53.912581, longitude: -122.747775},{name: "Whistler", latitude: 50.116003, longitude: 122.959426},{name: "Revelstoke", latitude: 50.997047, longitude:-118.197822},{name: "Creston", latitude: 49.095543 , longitude: -116.503134}]
+
 export default function Weather() {
   //longitude and latitude of bc so we can set that in the viewport of the map
   const [viewport,setViewport] = useState({
