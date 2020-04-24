211c37f6184b6e017d0cc32573b693c1618f4aaa
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-24 15:45:42 -0700

    structured popup into finer divs

--- client/src/components/PopupInfo.js
+++ client/src/components/PopupInfo.js
@@ -5,19 +5,23 @@ export default function PopupInfo(props) {
 const days = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
 const day = new Date()
   return (
-      <div className="popup" onClick={props.onClick}>
-        <div className="popup__location">
-          <p>{props.location.name},</p>
-          <p>{`${days[day.getDay()]} ${props.current.observation_time}`}</p>
-          <p>{props.current.weather_descriptions[0]}</p>
-
+      <div className="popup">
+        <div className="popup__left">
+          <div className="popup__location">
+            <p>{props.location.name},</p>
+            <p>{`${days[day.getDay()]} ${props.current.observation_time}`}</p>
+            <p>{props.current.weather_descriptions[0]}</p>
+          </div>
+          <div className="popup__info">
+          <img className="popup__img" src={props.current.weather_icons[0]} />
+            <div className="popup__info--temp">
+              <p>{props.current.temperature}</p>
+              <div>°C</div>
+            </div>          
+          </div>
         </div>
-        <img className="popup__img" src={props.current.weather_icons[0]} />
-        <div className="popup__info">
-          <div className="popup__info--temp">
-            <p>{props.current.temperature}</p>
-            <div>°C</div>
-          </div>          
+        <div className="popup__right">
+
         </div>
       </div>
   )
