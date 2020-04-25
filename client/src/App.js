192cf3ee772bba370ee5fb8aa47b5b2c9cb6bea3
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-25 15:24:32 -0700

    added a sidebar and stykled it a little bit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Weather from "./components"
 
 function App() {
   return (
     <div className="App">
+      {/*Sidebar added so we can add more options and functionalities to the web app at a later stage*/}
+      <div className="sidebar">
+        <h1>SpeedWeather</h1>
+      </div>
         <Weather/>
     </div>
   );
